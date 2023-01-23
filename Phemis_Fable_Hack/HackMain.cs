7e43220f7ee3d0a4f9e816f488066b8886ff303a
ssalandary <86086118+ssalandary@users.noreply.github.com>
2023-01-22 20:30:41 -0800

    Add project files.

---
+++ Phemis_Fable_Hack/HackMain.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+namespace Phemis_Fable_Hack
+{
+    partial class HackMain : MonoBehaviour
+    {
+        public void Start()
+        {   // This function is called once for each instance of this class,
+            // when it starts execution (in this case, attached to an object)
+        }
+
+        public void Update()
+        {   // This function is called once per frame, it's frequency depends on the frame rate.
+            // This is at the beginning of the game logic cycle.
+        }
+        public void LateUpdate()
+        {   // This function is called once per frame, it's frequency depends on the frame rate.
+            // This is at the end of the game logic cycle.
+        }
+
+        public void OnGUI()
+        {   // This function is called at the end of the frame, after all game logic.
+            // It is called twice per frame: Once for rendering, and once for GUI Events
+            // This is where we do all drawing operations
+        }
+
+
+        /* - Physics Method - */
+        public void FixedUpdate()
+        {   // This function is called at a fixed frequency (Typically 100hz) and is independent of the frame rate.
+            // For physics operations.
+        }
+    }
+}
---
+++ Phemis_Fable_Hack/Loader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+namespace Phemis_Fable_Hack
+{
+    public class Loader
+    {
+        // Create a new GameObject
+        // In unity, a GameObject does not have a physical body, it is a parent 'container'
+        // The GameObject can be assigned components, 'Children'
+        private static readonly GameObject MGameObject = new GameObject();
+
+        public static void Load()
+        { // The function that our injector calls inside our target process
+
+            // Here, we add a Component 'Child' to our game object.
+            // For example is we had the reference to this component we could use functions like:
+            // ... HackMain.GetComponentInParent<type>() to retrieve other components attacked to the same object
+            // ... aka 'Sibling Components'
+            MGameObject.AddComponent<HackMain>();
+
+            // We tell Object (Our universal base class) that we do not want this component to need initializing
+            // ... on every scene load. Otherwise the object would be destroyed by the engine when we change scenes,
+            // ... This assumes that we would make another instance of this object when we change maps,
+            // ... go to the main menu, etc. (Change scenes)
+            UnityEngine.Object.DontDestroyOnLoad(MGameObject);
+        }
+        public static void Unload()
+        { // Destroys the Created object, again called by our injector to "eject" the assembly.
+            UnityEngine.Object.Destroy(MGameObject); //Destroys all instances of the object.
+        }
+    }
+}
